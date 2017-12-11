b1d2094dff3ca48b8fd338a93f3701626f58e2e6
Dangkey <hillisnillig@gmail.com>
2017-12-11 23:10:07 +0000

    Add files via upload

---
+++ Verkefni 1 queries.sql
@@ -0,0 +1,114 @@
+-- ********************** -- Skrifið eftirfarandi stored procedures: --**********************
+
+-- 1:	CourseList()
+-- Birtir lista(yfirlit) af öllum áföngum sem geymdir eru í gagnagrunninum.
+-- Áfangarnir eru birtir í stafrófsröð 
+	Delimiter //
+create procedure Courselist()
+	begin
+		select * from courses
+		order by courseNumber;
+	end //
+Delimiter ;
+-- 2:	SingleCourse()
+-- 	Birtir upplýsingar um einn ákveðin kúrs.
+--  Færibreytan er áfanganúmerið
+Delimiter //
+create procedure SingleCourse(course char(10))
+    begin
+		select * from Courses
+		where coursenumber = course;
+	end //
+Delimiter ;
+
+-- 3:   NewCourse()
+--  Nýskráir áfanga í gagnagrunninn.
+--  Skoðið ERD myndina til að finna út hvaða gögn á að vista(hvaða færibreytur á að nota)
+--  NewCourse er með out parameterinn number_of_inserted_rows sem skilar fjölda þeirra
+--  raða sem vistaðar voru í gagnagrunninum.  Til þess notið þið MySQL function: row_count()	
+Delimiter //
+create procedure NewCourse(
+in course_number char(10),
+in course_name varchar(75),
+in course_credits int,
+OUT number_of_inserted_rows int)
+    begin    
+    Insert into courses
+    values(course_number,course_name,course_credits);
+    set number_of_inserted_rows = row_count();
+end //
+Delimiter ;
+
+call NewCourse('GSF','GSF',3,@v); SELECT @v AS 'Number of Inserted Rows';
+   
+
+-- 4:	UpdateCourse()
+--  Hér eru notaðar sömu færibreytur og í lið 3.  Áfanganúmerið er notað til að uppfæra réttan kúrs.alter
+-- row_count( fallið er hér notað líka.
+
+	Delimiter //
+create procedure UpdateCourse(old_course_number char(10),course_number char(10),course_name varchar(75),course_credits int)
+	begin
+    update courses
+    set coursenumber = course_number,coursecredits = course_credits,coursename = course_name
+    where old_course_number = coursenumber;
+    end //
+    delimiter ;
+	
+	call UpdateCourse('For','GSF','GSF',3);
+-- 5:	DeleteCourse()
+-- Áfanganúmer(courseNumber) er notað hérna til að eyða réttum kúrs.
+-- ATH: Ef verið er að nota kúrsinn einhversstaðar(sé hann skráður á TrackCourses) þá má EKKI eyða honum.
+-- Sé hins vegar hvergi verið að nota hann má eyða honum úr Courses töflunni og einnig Restrictors töflunni.alter
+-- sem fyrr er out parameter notaður til að "skila" fjölda þeirra raða sem eytt var úr töflunni COurses
+
+	Delimiter //
+	create procedure DeleteCourse(course_number char(10))
+	begin
+    Delete from courses
+    where coursenumber = course_number;
+    end //
+    delimiter ;
+    
+
+-- ********************** -- Skrifið eftirfarandi functions: --**********************
+
+-- 6:	NumberOfCourses()
+-- fallið skilar heildarfjölda allra áfanga í grunninum
+	Delimiter //
+create Function NumberOfCourses()
+    RETURNS int
+    begin
+    declare Amount_of_Corses int;
+    set Amount_of_Corses = (
+    select count(*)
+    from courses);
+    return Amount_of_Corses;
+end //
+Delimiter ;
+
+
+-- 7:	TotalTrackCredits()
+--  Fallið skilar heildar einingafjölda ákveðinnar námsbrautar(track)
+--  Senda þarf trackID inn sem færibreytu
+	create function TotalTrackCredits(trackid int)
+    returns int
+    begin
+    select coursecredits from courses
+join trackcourses
+on trackcourses.trackID = courses.trackID
+    where 
+
+
+-- 8:   HighestCredits()
+-- Fallið skilar einingafjölda þess námskeiðs(þeirra námskeiða) sem hafa flestar eininar.
+-- ATH:  Það geta fleiri en einn kúrs verið með sama einingafjöldann. :að á ekki að hafa 
+-- áhfri á niðurstöðuna.
+
+
+-- 9:  TopTracksDivision()
+-- Fallið skilað toppfjölda námsbrauta(tracks) sem tilheyra námsbrautum(Divisions)
+
+-- 10:  leastRestrictedCourseNumber()
+-- Fallið skilar minnsta fjölda kúrsa í Restrictors töflunni.
+-- ATH:  Ef kúrs eða kúrsar eru t.d. með einn undanfara þog ekkert meir þá myndi fallið skila 1 
---
+++ Verkefni 2 queries (1).sql
@@ -0,0 +1,114 @@
+-- 1. Hannið viðbætur við ProgressTracker gagnagrunninn þannig að nú sé hægt að skrá nemanda og að
+-- sá nemandi geti valið sér námsleið(innan námsbrautar).
+
+	create table students(
+		student_id int(11) not null primary key auto_increment,
+		name varchar(40) not null,
+		age int(11),
+		email varchar(30) unique key,
+		phoneNo varchar(7),
+		schoolID int(11) default 1,
+		divisionID int(11),
+		trackID int(11)
+	);
+	INSERT INTO `students` (`student_id`, `name`, `age`, `email`, `phoneNo`, `schoolID`, `divisionID`, `trackID`) VALUES
+	(1, 'Hilmar', 18, 'hillisnillig@gmail.com', '8455622', 1, 11, 9),
+	(2, 'James', 18, 'bla', '8455622', 1, 11, 9),
+	(3, 'Stella', 18, 'abc', '8455622', 1, 11, 9),
+	(4, 'Jenny', 18, 'qwerty', '8455622', 1, 11, 9);
+	
+	create table studentCourses(
+		studentID int(11),
+		courseNumber char(10),
+		mandatory  char(10),
+		lokið tinyint(1),
+		primary key (studentID , CourseNumber),
+		foreign key (studentID) references students(student_id)
+	);
+	INSERT INTO `studentcourses` (`studentID`, `courseNumber`, `Lokið`) VALUES
+	(1, 'EÐL403', 1),
+	(1, 'FOR3G3U', 1),
+	(1, 'GSF3A3U', 1),
+	(1, 'STÆ403', 1),
+	(2, 'EÐL203', 0),
+	(2, 'FOR3D3U', 1),
+	(2, 'GSF2B3U', 1),
+	(3, 'EÐL303', 1),
+	(3, 'GSF2B3U', 0),
+	(4, 'EÐL103', 1);
+
+	Delimiter //
+	create procedure AddStudent(
+		name varchar(40),
+		age int(11),
+		email varchar(30),
+		phoneNo varchar(7),
+		schoolID int(11),
+		divisionID int(11),
+		trackID int(11)
+		)
+	begin
+		insert into students(name,age,email,phoneNo,schoolID,divisionID,trackID)
+		Values(name,age,email,phoneNo,schoolID,divisionID,trackID);
+	end //
+	Delimiter ;
+	-- test student
+	call AddStudent('Davíð',18,'Davíð@gmail.com','8455622',1,11,9);
+	
+	
+-- 2. Skrifið trigger fyrir insert aðgerð í töfluna Restrictors. Triggerinn kemur í veg fyrir að
+-- courseNumber og RestrictorID séu sami kúrsinn. Sé um sama kúrs að ræða, kastar triggerinn villu,
+-- skrifar út villutexta og kemur í veg fyrir insert.
+
+Delimiter //
+create trigger before_resistors_insert
+before insert on restrictors
+for each row
+begin
+    if (NEW.courseNumber = NEW.restrictorID) then
+		signal sqlstate '45000' set message_text = 'The course number cannot be the same as the Restrictor ID';
+    end if;
+end //
+delimiter ;
+
+-- 3. Skrifið samskonar trigger fyrir update aðgerð í töfluna Restrictors.
+
+Delimiter //
+create trigger before_resistors_update
+before update on restrictors
+for each row
+begin
+    if (NEW.courseNumber = NEW.restrictorID) then
+		signal sqlstate '45000' set message_text = 'The course number cannot be the same as the Restrictor ID';
+    end if;
+end //
+delimiter ;
+
+update restrictors set courseNumber  = 'GSF', restrictorID = 'GSF', restrictorType = 1 where restrictorID = 'STÆ103'
+
+-- 4. Skrifið stored procedure sem leggur saman allar einingar sem nemandinn hefur lokið á ákv. námskeiðum
+
+Delimiter //
+create procedure samtals_einingar(
+student int(11)
+)
+begin
+select sum(courseCredits) from courses
+join studentcourses
+on studentcourses.courseNumber = courses.courseNumber
+join students
+on studentcourses.studentID = students.student_ID
+where studentID = student and studentcourses.lokið = 1;
+end //
+delimiter ;
+
+call samtals_einingar(2);
+
+-- 5. Skrifið cursor sem sækir alla skylduáfanga sem nemandi þarf að taka og setur þá í töfluna sem
+-- geymir áfanga sem hann hefur valið. Setjið cursorinn í stored procedure sem gæti heitið eitthvað
+-- "AddMandtoryCourses” og er keyrður þegar nemandinn velur sér námsleið(sú aðgerð gæti verið í
+-- SP sem heitir "NewStudent”)
+
+
+
+
