65a3993054934fbdab5fe48d5592a0c6a0bf7767
elas99 <elas99@hotmail.com>
2017-12-12 23:41:47 +0000

    Add files via upload

---
+++ verkefni4.sql
@@ -0,0 +1,93 @@
+Insert Skipun
+Delimiter //
+	create procedure AddStudent(
+		name varchar(40),
+		age int(11),
+		email varchar(30),
+		phoneNo varchar(7),
+		schoolID int(11),
+		divisionID int(11),
+		trackID int(11)
+		)
+	begin
+		insert into students(name,age,email,phoneNo,schoolID,divisionID,trackID)
+		Values(name,age,email,phoneNo,schoolID,divisionID,trackID);
+	end //
+	Delimiter ;
+	-- test student
+	call AddStudent('Davíð',18,'Davíð@gmail.com','8455622',1,11,9);
+
+Select Skipun
+Delimiter //
+
+create procedure SelectStudents(
+
+studentname varchar(50)
+
+)
+	
+begin
+	
+select * from students
+   
+ where name = studentname;
+  
+  end //
+  
+  delimiter ;
+    
+
+
+call SelectStudents('Hilmar')
+
+
+
+Update skipun
+Delimiter //
+
+create procedure UpdateStudents(
+
+oldstudentname varchar(50),
+
+studentname varchar(50)
+)
+
+begin
+    
+update students
+    
+set name = studentname
+    
+where oldstudentname = name;
+    
+end //
+  
+  delimiter ;
+	
+
+call UpdateStudents('Hilmar','Bob');
+
+
+
+Delete Skipun
+
+Delimiter //
+
+create procedure DeleteStudents(
+
+studentname varchar(50)
+
+)
+	
+begin
+	
+delete from students
+   
+ where name = studentname;
+  
+  end //
+  
+  delimiter ;
+
+
+call DeleteStudents('Jenny')
